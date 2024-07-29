664a055890051eba530822c624a6d9e479c928d9
Mujtaba_Sultan <b.mojtabaali@gmail.com>
2024-07-29 20:38:13 +0300

    nearly fixed the edit for other users

--- controllers/story-cntrl.js
+++ controllers/story-cntrl.js
@@ -6,6 +6,7 @@ const User = require("../models/user");
 router.get("/", async (req, res) => {
   try {
     const currentUser = await User.findById(req.session.user._id);
+
     res.render("crud/index.ejs", {
       stories: currentUser.stories,
     });
@@ -17,8 +18,9 @@ router.get("/", async (req, res) => {
 
 router.get("/view", async (req, res) => {
   try {
+    const currentUser = await User.findById(req.session.user._id);
     const users = await User.find({});
-    res.render("crud/all.ejs", { users });
+    res.render("crud/all.ejs", { users, currentUser });
   } catch (error) {
     console.log(error);
     res.redirect("/");
@@ -50,14 +52,34 @@ router.post("/", async (req, res) => {
 
 router.get("/:crudId", async (req, res) => {
   try {
+    const currentUser = await User.findById(req.session.user._id);
+    let userWithStory;
     const users = await User.find({});
-    const currentUser = await User.findById(req.params.crudId);
-    const stories = currentUser.stories.id(req.params.crudId);
+    let story = null;
+    let compare = 2;
+
+    for (let user of users) {
+      story = user.stories.id(req.params.crudId);
+      userWithStory = users.find((user) => user.stories.id(req.params.userId));
+      if (story) {
+        compare = user;
+        break;
+      }
+    }
+
+    compare1 = compare.id;
+    compare2 = currentUser.id;
+    console.log(compare1);
+    console.log(compare2);
+    // const creater = userWithStory.id;
 
     res.render("crud/show.ejs", {
-      stories,
+      story,
       users,
       currentUser,
+      compare1,
+      compare2,
+      // creater,
     });
   } catch (error) {
     console.log(error);
@@ -80,12 +102,16 @@ router.delete("/:crudId", async (req, res) => {
 router.get("/:crudId/edit", async (req, res) => {
   try {
     const currentUser = await User.findById(req.session.user._id);
-    const story = currentUser.stories.id(req.params.crudId);
-    const formattedDate = story.date.toISOString().split("T")[0];
-    res.render("crud/edit.ejs", {
-      story,
-      formattedDate,
-    });
+    if (req.params.userId == currentUser) {
+      const story = currentUser.stories.id(req.params.crudId);
+      const formattedDate = story.date.toISOString().split("T")[0];
+      res.render("crud/edit.ejs", {
+        story,
+        formattedDate,
+      });
+    } else {
+      res.redirect("/:crudId/edit/public");
+    }
   } catch (error) {
     console.log(error);
     res.redirect("/");
@@ -93,6 +119,45 @@ router.get("/:crudId/edit", async (req, res) => {
 });
 
 router.put("/:crudId/edit", async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    req.body.date = new Date(req.body.date);
+
+    if (req.body.editable === "on") {
+      req.body.editable = true;
+    } else {
+      req.body.editable = false;
+    }
+    await currentUser.stories.id(req.params.crudId).set(req.body);
+    await currentUser.save();
+    res.redirect("/");
+  } catch (error) {
+    console.log(error);
+    res.redirect("/");
+  }
+});
+
+router.get("/:crudId/edit/public", async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    creater = await User.findOne({
+      stories: { $elemMatch: { _id: req.params.crudId } },
+    });
+    const story = creater.stories.id(req.params.crudId);
+    //formattedDate = currentUser.stories.date.toISOString().split("T")[0];
+
+  
+    res.render("crud/editp.ejs", {
+      story,
+      // formattedDate,
+    });
+  } catch (error) {
+    console.log(error);
+    res.redirect("/");
+  }
+});
+
+router.put("/:crudId/edit/public", async (req, res) => {
   try {
     const currentUser = await User.findById(req.session.user._id);
     req.body.date = new Date(req.body.date);
